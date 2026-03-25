36b5710c76f4a445811c92e5cdc37953594f989f
Francesco Maria Tripodi <fm.tripodi@almaviva.it>
2026-03-25 18:08:14 +0100

    commit

---
+++ src/main/java/com/qtm/commonlib/dto/UserRoleProjectDto.java
@@ -0,0 +1,14 @@
+package com.qtm.commonlib.dto;
+
+import lombok.Data;
+
+/**
+ * DTO condiviso per l'associazione utente-tenant-ruolo-progetto.
+ */
+@Data
+public class UserRoleProjectDto {
+    private Long userId;
+    private Long tenantId;
+    private String roleId;
+    private String projectId;
+}
