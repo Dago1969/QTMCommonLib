9e77e964facb24faab1db12b1941e1b85dc2a893
Francesco Maria Tripodi <fm.tripodi@almaviva.it>
2026-09-11 12:48:49 +0200

    caricamento ASL

--- src/main/java/com/qtm/commonlib/dto/ASLDto.java
+++ src/main/java/com/qtm/commonlib/dto/ASLDto.java
@@ -14,6 +14,7 @@ import lombok.NoArgsConstructor;
 @Builder(toBuilder = true)
 public class ASLDto {
     private Long id;
+    private Integer anno;
     private String codiceAzienda;
     private String denominazioneAzienda;
     private Long cityId;
